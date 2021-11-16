7a34245986600533d595ce45260be04cfb28b605
edgardopina <edgardopina@yahoo.com>
2021-11-15 23:45:55 -0700

    Created Pizza schema and model, package up Pizza model in /models/index.js, added mongoose.connect to heroku/local database, added logging of mongoose queries.

---
+++ models/index.js
@@ -0,0 +1,5 @@
+//! this file will package up all the app models
+
+const Pizza = require('Pizza');
+
+module.exports = { Pizza };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+// get Mongoose connect when we start the app
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/pizza-hunt', {
+   // useFindAndModify: false,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+mongoose.set('debug', true); // log mongo queries being executed
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
