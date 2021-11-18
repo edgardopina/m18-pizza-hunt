bb0dd8e7e45cb975a3f474834bafc1aa94f9c0d6
edgardopina <edgardopina@yahoo.com>
2021-11-17 22:09:56 -0700

    Added fetch() call to pizza-list.js to display akk pizzas; Added getter to format createdAt field in Pizza.js model and enabled getters: true. TESTED.

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,13 @@
 const { Schema, model } = require('mongoose');
 
+
 const CommentSchema = new Schema({
    writtenBy: { type: String },
    commentBody: { type: String },
-   createdAt: { type: Date, default: Date.now },
+   createdAt: {
+      type: Date,
+      default: Date.now(),
+   },
 });
 
 const Comment = model('Comment', CommentSchema);
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,11 +1,17 @@
-const { Schema, model } = require('mongoose'); // import Schema ctor and model function
+const { Schema, model } = require('mongoose'); //* import Schema ctor and model function
+const dateFormat = require('../utils/dateFormat');
 
 //! create Pizza schema
 const PizzaSchema = new Schema(
    {
       pizzaName: { type: String },
       createdBy: { type: String },
-      createdAt: { type: Date, default: Date.now() },
+      createdAt: {
+         type: Date,
+         default: Date.now(),
+         //* define getter; each time we retrieve createdAt, it will be formatted by dateFormat()
+         get: createdAtVal => dateFormat(createdAtVal),
+      },
       size: { type: String, default: 'Large' },
       toppings: [],
       comments: [
@@ -15,10 +21,10 @@ const PizzaSchema = new Schema(
          },
       ],
    },
-   //* enable mongoose virtuals for this schema
    {
       toJSON: {
-         virtuals: true,
+         virtuals: true, //* enable mongoose virtuals for this schema
+         getters: true, //* enable mongoose getters for this schema
       },
       id: false, //* mongoose virtuals do not need id
    }
@@ -32,4 +38,4 @@ PizzaSchema.virtual('commentCount').get(function () {
 //! create Pizza model using PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-module.exports = Pizza ; // exports the Pizza model
+module.exports = Pizza; // exports the Pizza model
