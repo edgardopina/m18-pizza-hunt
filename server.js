ff849bde3739087ff66014c42b6967146b2f5711
edgardopina <edgardopina@yahoo.com>
2021-11-19 15:27:43 -0700

    Commented useFindAndModify

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ app.use(require('./routes'));
 
 // get Mongoose connect when we start the app
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/pizza-hunt', {
-   useFindAndModify: false,
+   //useFindAndModify: false, //! use only for mongoose version 6+
    useNewUrlParser: true,
    useUnifiedTopology: true,
 });
 mongoose.set('debug', true); // log mongo queries being executed
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-   
